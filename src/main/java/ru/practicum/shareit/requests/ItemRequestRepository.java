7054e0822540459d099673a857572aaad5087c61
Андрей Барышников <andrey1andrey@narod.ru>
2022-08-28 13:36:42 +0300

    14

--- src/main/java/ru/practicum/shareit/requests/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/requests/ItemRequestRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.requests;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.requests.model.ItemRequest;
 
@@ -12,14 +11,6 @@ import java.util.List;
 @Repository
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
     List<ItemRequest> findAllByRequesterIdOrderByIdAsc(long requester_id);
-
-    @Query(nativeQuery = true, value = "WITH temp AS " +
-        "(SELECT id, description, requester_id, created, ROW_NUMBER() OVER (ORDER BY created) AS line_number " +
-        "FROM item_requests " +
-        "WHERE requester_id != ?) " +
-        "SELECT * FROM temp WHERE line_number >= ? " +
-        "LIMIT ?;")
-    List<ItemRequest> findOthersRequests(long user, int from, int size);
 
     Page<ItemRequest> findByIdIsNot(long requesterId, Pageable pageable);
 }
