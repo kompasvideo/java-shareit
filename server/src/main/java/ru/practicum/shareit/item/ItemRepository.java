1c3a3c4930e257e3724384bdb35585f4250a4a7d
Андрей Барышников <andrey1andrey@narod.ru>
2022-09-18 15:32:35 +0000

    4

--- server/src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -17,7 +17,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findItemsByOwnerId(long userId);
 
-    List<Item> findAllByOwnerId(long userId);
     List<Item> findAllByOwnerIdOrderByIdAsc(long userId);
 
     List<Item> findAllByRequestId(long requestId);
